ad225079432a9d1f6fee98f89a77114c9ef2c6ae
gero_ <gergesfaiz3@gmail.com>
2025-09-17 23:52:48 +0300

    chang button color

--- lib/xoGame/BoardButton.dart
+++ lib/xoGame/BoardButton.dart
@@ -4,8 +4,15 @@ import 'package:flutter/material.dart';
 class Boardbutton extends StatelessWidget {
   String label;
   int index;
-Function onClick;
-  Boardbutton({required this.index,required this.onClick,required this.label, super.key});
+  Function onClick;
+
+  Boardbutton({
+    required this.index,
+    required this.onClick,
+    required this.label,
+
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -15,12 +22,19 @@ Function onClick;
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
           ),
-          backgroundColor: Colors.blue.shade200,
+          backgroundColor: Colors.green.shade200,
         ),
         onPressed: () {
           onClick(index);
         },
-        child: Text(label, style: TextStyle(color: Colors.white,fontSize: 60,fontWeight: FontWeight.w100)),
+        child: Text(
+          label,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 60,
+            fontWeight: FontWeight.w100,
+          ),
+        ),
       ),
     );
   }
